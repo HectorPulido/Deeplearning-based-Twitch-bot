9bdf09c7f266740de92dd3ae060259823e98681c
Hector Pulido <hector.pulido@platzi.com>
2023-09-02 23:38:21 -0500

    naming fix

--- src/custom_events/__init__.py
+++ src/custom_events/__init__.py
@@ -3,6 +3,6 @@ from custom_events.welcome import welcome
 from custom_events.wet import wet
 from custom_events.pick_random_user import pick_random_user
 from custom_events.links import links
-from custom_events.BitMessage import BitMessage
-from custom_events.TalkToChatbot import TalkToChatbot
-from custom_events.TextToSpeech import TextToSpeech
+from custom_events.bit_message import BitMessage
+from custom_events.talk_to_chatbot import TalkToChatbot
+from custom_events.text_to_speech import TextToSpeech
